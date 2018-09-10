5ea8b6819e2fb31c3fb39be0f3b0bae3134ed2b1
Unknown <ruofu.sun@gmail.com>
2018-09-10 00:40:20 -0400

    defined the player variable and placed it onto screen

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4,8 +4,12 @@ import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	    var _player:Player;
 	override public function create():Void
 	{
+		//define the position of the player wrt the screen
+		_player = new Player(20, 20);
+	    add(_player);
 		super.create();
 	}
 
