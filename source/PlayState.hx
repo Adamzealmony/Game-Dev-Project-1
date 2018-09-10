9df724d78970a8564e7643edcaa4221ef2179a38
Sophie Richards <richas3@rpi.edu>
2018-09-10 17:08:19 -0400

    fixed PlayState
    the player data was deleted in the merge so I added it back, oops.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -6,12 +6,17 @@ import flixel.math.FlxMath;
 
 class PlayState extends FlxState
 {
+	var _player:Player;
 	var _hud:HUD;
 	var _score:Int=0;
 	var _stamina:Int = 100;
 	var _timer:FlxTimer;
 	override public function create():Void
 	{
+		//define the position of the player wrt the screen
+		_player = new Player(20, 20);
+	    add(_player);
+		//creates a hud and timer
 		_hud = new HUD();
 		_timer = new FlxTimer();
 		_timer.start();
