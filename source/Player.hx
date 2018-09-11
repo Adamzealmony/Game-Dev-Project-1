6cb05942715425d073017e4f8e54ba870c1d4c23
Unknown <ruofu.sun@gmail.com>
2018-09-10 22:52:55 -0400

    When SHIFT and one of the arrow key is pressed, player can rush

--- source/Player.hx
+++ source/Player.hx
@@ -16,6 +16,7 @@ import flixel.math.FlxPoint;
 	     public  var _down:Bool = false;
 	     public  var _left:Bool = false;
 	     public  var _right:Bool = false;
+		 public var _rush:Bool = false;
 		 
 		 override public function update(elapsed:Float):Void 
 		 {
@@ -50,6 +51,7 @@ import flixel.math.FlxPoint;
 		 _down = FlxG.keys.anyPressed([DOWN, S]);
 		 _left = FlxG.keys.anyPressed([LEFT, A]);
 		 _right = FlxG.keys.anyPressed([RIGHT, D]);
+		 _rush = FlxG.keys.anyPressed([SHIFT]);
 		 //cancel the effect of opposing input 
 		 if (_up && _down)
 		 _up = _down = false;
@@ -57,9 +59,11 @@ import flixel.math.FlxPoint;
 		 _left = _right = false;
 		 //if player is acturally moving 
 		 if (_up || _down || _left || _right)
-		 {//creat a variable to hold the angle of our player
+		 {
+	     //creat a variable to hold the angle of our player
 		 animation.play("Walk");
 		 var mA:Float = 0;
+		
 		 if(_up)
 		 {
 			mA = -90;			
@@ -80,9 +84,19 @@ import flixel.math.FlxPoint;
 		 velocity.set(speed, 0);
 		 //rotate that point around(0,0) by mA degrees
 		 //a weak FlxPoint is recycled once it is used in a haxeflixel function
+		 velocity.rotate(FlxPoint.weak(0, 0), mA);
+		   if (_rush) 
+		 {
+		 velocity.set(speed * 5, 0);
 		 velocity.rotate(FlxPoint.weak(0,0),mA);
 		 
 		 }
+		 
+		 
+		 }
+		 
+		
+		 
 		 
 	 }
 	 
