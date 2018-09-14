3541a40987afb276882cd9622036d950ae46d793
Sophie Richards <richas3@rpi.edu>
2018-09-13 23:34:42 -0400

    Stamina bar updates again

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -34,8 +34,9 @@ class PlayState extends FlxState
 		add(level.objectsLayer);
 		//define the position of the player wrt the screen
 		_player = new Player(105, 105);
-		_stamBar = new FlxBar(0, 0, LEFT_TO_RIGHT, 100, 10, _player);
+		_stamBar = new FlxBar(0, 0, LEFT_TO_RIGHT, 100, 10, _player, "_stamina");
 		_stamBar.percent = _stamina;
+		_stamBar.trackParent(0, 100);
 	    add(_player);
 		add(_stamBar);
 		//creates a hud and timer
@@ -44,7 +45,6 @@ class PlayState extends FlxState
 		_timer.start();
 		//change .time to however long the player has
 		_timer.time = 100;
-		_stamBar.trackParent(0, 100);
 		add(_hud);
 		
 		
@@ -53,21 +53,10 @@ class PlayState extends FlxState
 
 	override public function update(elapsed:Float):Void
 	{
-		if (_timer.timeLeft <= .1){
+		 if (_timer.timeLeft <= .1){
 			FlxG.switchState(new GameOverState());
 			return;
 		}
-		if (_player._rush&& _player._canRush&&_stamina>0){
-			_stamina -= 1;
-		}
-		else if(_stamina!=100){
-			_stamina += 1;
-			_player.updateRush(false);
-		}
-		else if (_stamina == 100){
-			_player.updateRush(true);
-		}
-		_stamBar.percent = _stamina;
 		FlxG.camera.follow(_player);
 		_hud.updateHUD(FlxMath.roundDecimal(_timer.timeLeft, 0), _score);
 		super.update(elapsed);
