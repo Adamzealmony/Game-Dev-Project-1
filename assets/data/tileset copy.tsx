4a4d438be61448e8e5c01db6079db0265248e9ff
Hakim-a-barber <42918373+Hakim-a-barber@users.noreply.github.com>
2018-09-20 23:20:10 -0400

    deleted the old tsx file for our 1st tileset

--- assets/data/tileset copy.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tileset copy" tilewidth="100" tileheight="100" tilecount="6" columns="3">
- <image source="tileset.png" width="300" height="200"/>
-</tileset>
